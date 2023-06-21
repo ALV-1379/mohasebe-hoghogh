e75980afbfe92d82c4cf4b5b744d53ee639f2d12
Mohamad alavi <Mohamad alavi@DESKTOP-9QCQADM>
2023-06-21 09:09:19 -0700

    Add project files.

---
+++ mohasebe-hoghogh/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mohasebe_hoghogh
+{
+    class Class1
+    {
+        private int a;
+        private int b;
+        private int c;
+
+        public double hoghogh(int a , int b , int c)
+        {
+            int x = (a - b);
+            int y = (x - ((x * c) / 100));
+            return y;
+        }
+
+    }
+}
---
+++ mohasebe-hoghogh/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace mohasebe_hoghogh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("meghdar hoghogh ra vared konid");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("meghdar hazine bime ra vared konid");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("darsad maliat ra vared konid");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+
+            Class1 mahiane = new Class1();
+
+            Console.WriteLine($"hoghogh khales shoma ={mahiane.hoghogh(a ,b , c)}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
